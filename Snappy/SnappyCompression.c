b2380c909e6538f0f5d2ec3f87dd25518dd779b3
Riccardo Belli <riccardo.belli@student.supsi.ch>
2019-12-01 10:21:05 +0100

    Leggo dimensione file originale

--- Snappy/SnappyCompression.c
+++ Snappy/SnappyCompression.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
-#include <stdint-gcc.h>
 #include "IOUtils.h"
 
 
+void parse_to_varint(int size) {
+
+}
 
 int main() {
     char readByte;
     FILE *input;
     if((input = fopen("C:\\Users\\belli\\Documents\\Archivio SUPSI\\SnappyProject\\asd20192020tpg3\\Snappy\\testWikipedia.txt", "r") )!= NULL){
+
+        fseek(input, 0, SEEK_END);
+        int file_size = ftell(input);
+        fseek(input, 0, SEEK_SET);
+        parse_to_varint(file_size);
+        printf("%d\n", file_size);
         while( (fread(&readByte, sizeof(char), 1, input))!=0 ){
             printf("%X ", readByte);
-            printf("%c ", readByte);
         }
     } else {
-        puts("Errore lettura file");
+        puts("Errore apertura file");
     }
 }
