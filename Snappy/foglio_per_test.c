82729b44864cfc8c9cd7fc00457ed9b1836cd0e4
Riccardo Belli <riccardo.belli@student.supsi.ch>
2019-12-09 16:39:23 +0100

    Prima versione funzionante di write_literal() e write_copy()

--- Snappy/foglio_per_test.c
+++ Snappy/foglio_per_test.c
@@ -4,9 +4,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-
-int main(){
-
+void prova_con_BTS(){
     srand(time(NULL));
     Tree *hash_table[10];
     for (int i = 0; i < 10; ++i) {
@@ -21,6 +19,39 @@ int main(){
         print_tree_inorder(hash_table[i]);
         printf("\n");
     }
+}
+
+int main(){
+    unsigned char *input = "\x3F""\xEC""\xAD""\x09";
+
+    u32 load;
+    load = (input[0] << 24u) | (input[1] << 16u) | (input[2] << 8u) | input[3];
+    printf("%X\n", load);
+    unsigned int len = 67;
+    unsigned char output;
+    if(len < 60){//TODO: generalizzare
+        output = ((len-1) << 2u) & 0xFF;
+        printf("%X", output);
+    } else {
+        output = 60 << 2u;
+        printf("%X ", output);
+        output = (len-1) & 0xFF;
+        printf("%X", output);
+
+    }
+    puts("");
+    len = 7;
+    unsigned long offset = 63;
+    output = (len-1) << 2 | 2;//TODO 3 tipi di copia
+    printf("%X ", output);
+
+    output = offset & 0xFF;
+    printf("%X ", output);
+
+    output = (offset >> 8u) & 0xFF;
+    printf("%X ", output);
+
+
 
 
 
--- Snappy/snappy_compression.c
+++ Snappy/snappy_compression.c
@@ -4,7 +4,20 @@
 #include "varint.h"
 #include "BST.h"
 
-static unsigned int htable_size = 14;
+static unsigned int htable_size = 20;
+
+int find_copy_length(char *input, char *candidate, const char *limit) {//TODO: max copy length? Incremental copy?
+    int length = 0;//TODO ottimizzare partendo da +4
+    for(;input <= limit; input++, candidate++){
+        if(*input==*candidate){
+            length++;
+        } else {
+            return length;
+        }
+    }
+
+    return length;
+}
 
 static inline u32 hash_bytes(u32 bytes){
     u32 kmul = 0x1e35a7bd;
@@ -17,28 +30,52 @@ Tree **get_hash_table(int file_size) {
     return hash_table;
 }
 
-int getFileSize(const FILE *input) {
-    fseek(input, 0, SEEK_END);
-    int file_size = ftell(input);
-    fseek(input, 0, SEEK_SET);
+int getFileSize(const FILE *finput) {
+    fseek(finput, 0, SEEK_END);
+    int file_size = ftell(finput);
+    fseek(finput, 0, SEEK_SET);
     return file_size;
 }
 
-void emit_literal(u32 buffer[], int *len) {
-    printf("Literal di dimensione %d\n", *len);
-    for (int i = 0; i < *len; ++i) {
-        printf("%X ", buffer[i]);
+char *write_literal(const char *input, char *output, unsigned int len) {
+    printf("Literal di dimensione %d\n", len);
+    //Scrivo tag byte
+    if(len < 60){//TODO: generalizzare
+        *(output++) = (len << 2u) & 0xFF;
+    } else {
+        *(output++) = (60 << 2u);
+        *(output++) = (len-1) & 0xFF;
     }
-    *len = 0;
+    for (int i = 0; i < len; ++i) {
+        *(output++) = input[i];
+        printf("%X ", input[i]);
+    }
     printf("\n");
+    return output;
+
+}
+
+u32 get_next_u32(const unsigned char *input, const unsigned char *limit) {
+
+    return (input[0] << 24u) | (input[1] << 16u) | (input[2] << 8u) | input[3];
+}
+
+char *write_copy(unsigned int length, unsigned long offset, char *output) {
+    *(output++) = (length-1) << 2 | 2;//TODO 3 tipi di copia
+    *(output++) = offset & 0xFF;
+    *(output++) = (offset >> 8u) & 0xFF;
+    return output;
 
 }
 
 int main() {
-    u32 literal_buffer[300];
-    int literal_counter = 0;
-    u32 read_u32;
-    FILE *input;
+    int literal_length = 0;
+    int copy_length = 0;
+    FILE *finput;
+    char *input;
+    char *output;
+    const char * beginning;
+    char *input_limit;
 
 
     Tree *hash_table[htable_size];
@@ -46,29 +83,44 @@ int main() {
         hash_table[i] = create_tree();
     }
 
-    if((input = fopen("C:\\Users\\belli\\Documents\\Archivio SUPSI\\SnappyProject\\asd20192020tpg3\\Snappy\\testWikipedia.txt", "r") )!= NULL){
-
-        int file_size = getFileSize(input);
+    if((finput = fopen("C:\\Users\\belli\\Documents\\Archivio SUPSI\\SnappyProject\\asd20192020tpg3\\Snappy\\testWikipedia.txt", "r") )!= NULL){
+        int file_size = getFileSize(finput);
+        input = (char *)malloc(sizeof(char)*file_size);
+        output = (char *)malloc(sizeof(char)*file_size);
+        fread(input, sizeof(char), file_size, finput);
         printf("Dimesnione file: %d bytes, %d u32\n", file_size, file_size/4);
-
-        int index = 0;
-        Node *copy;
-        while( (fread(&read_u32, sizeof(u32), 1, input))!=0 ) {
-            index = hash_bytes(read_u32);
-            if (is_empty(hash_table[index]) | ((copy = find(read_u32, hash_table[index]) ) == NULL)) {
-                //printf("%X literal\n", read_u32);
-                literal_buffer[literal_counter++] = read_u32; //Aggiungo il literal in output
-                insert(read_u32, ftell(input)/4, hash_table[index]);
-            } else {
-                emit_literal(literal_buffer, &literal_counter);
-                printf("%X copy of %X\n", read_u32, copy->bytes);
-
-            }
-
-        }
-        fclose(input);
+        beginning = input;
+        input_limit = input + file_size;
     } else {
         puts("Errore apertura file");
+        return 1;
+    }
+    fclose(finput);
+
+    u32 current_u32 ;
+    int index = 0;
+    Node *copy;
+    char *candidate;
+    while( input+4 <= input_limit ) {
+        current_u32 = get_next_u32(input, input_limit);
+        index = hash_bytes(current_u32);
+        if (is_empty(hash_table[index]) | ((copy = find(current_u32, hash_table[index]) ) == NULL)) {
+            //printf("%X literal\n", current_u32);
+            //output[literal_length++] = current_u32; //Aggiungo il literal in output
+            insert(current_u32, input - beginning , hash_table[index]);
+            literal_length++;
+            copy_length = 0;
+        } else {
+            output = write_literal(input - literal_length, output, literal_length);
+            literal_length = 0;
+            candidate = beginning + copy->offset;
+            copy_length = find_copy_length(input, candidate, input_limit);
+            printf("%X copy of offset = %d and length = %d\n", current_u32, input - candidate, copy_length);
+            output = write_copy(copy_length, input - candidate, output);
+
+        }
+        input+= 4 + copy_length;
+
     }
     puts("\n");
     for (int i = 0; i < htable_size; ++i) {
@@ -78,3 +130,5 @@ int main() {
 
 
 }
+
+
