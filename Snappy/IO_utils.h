57183792da3759765c97e1c8f38eadb65d3ad1ed
Riccardo Belli <riccardo.belli@student.supsi.ch>
2020-01-06 11:06:24 +0100

    Aggiunte funzioni per apertura file

--- Snappy/IO_utils.h
+++ Snappy/IO_utils.h
@@ -2,5 +2,9 @@
 #define SNAPPY_IO_UTILS_H
 
 void print_char_as_bits(char c);
+FILE *open_read(char *file_name);
+FILE *open_write(char *file_name);
+FILE *open_append(char *file_name);
+unsigned long long get_size(FILE *file);
 
 #endif
